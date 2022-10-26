838eb26f607f6ba413c413556d189b1e1493352d
liyunhe <liyunhe@gridsum.com>
2022-10-26 17:23:31 +0800

    build: update base path

--- vue.config.js
+++ vue.config.js
@@ -16,9 +16,10 @@ function resolve(dir) {
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/form-generator/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/form-generator/'
+  //   : '/',
+  publicPath: '/',
   pages: {
     index: {
       entry: 'src/views/index/main.js',
